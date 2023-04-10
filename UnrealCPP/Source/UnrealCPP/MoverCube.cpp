35e324a429caa828aab1d96dc944a7b2ada0c7c7
Arvid van den Hoogen <arvidvdhoogen@gmail.com>
2023-04-10 13:13:25 +0200

    added auto quit feature + finished cpp project

--- UnrealCPP/Source/UnrealCPP/MoverCube.cpp
+++ UnrealCPP/Source/UnrealCPP/MoverCube.cpp
@@ -1,5 +1,7 @@
 #include "MoverCube.h"
 
+#include "Kismet/GameplayStatics.h"
+
 AMoverCube::AMoverCube()
 {
 	PrimaryActorTick.bCanEverTick = true;
@@ -16,6 +18,44 @@ void AMoverCube::BeginPlay()
 void AMoverCube::Tick(float DeltaTime)
 {
 	AActor::Tick(DeltaTime);
-
+	//mover cube behaviour
+	MoveToNearestNeighbour(DeltaTime);
+	MoveRandomOffset(DeltaTime);
 }
 
+void AMoverCube::MoveToNearestNeighbour(float DeltaTime) {
+	//find all actors of type
+	TArray<AActor*> movers;
+	UWorld* world = GetWorld();
+	if (world) {
+		UGameplayStatics::GetAllActorsOfClass(world, AMoverCube::StaticClass(), movers);
+		//remove close movers
+		for (int i = movers.Num() - 1; i >= 0; i--) {
+			if ((GetActorLocation() - movers[i]->GetActorLocation()).Length() < 10.0f) {
+				movers.RemoveAt(i);
+			}
+		}
+		if (movers.Num() > 0) {
+			//sort list based on distance
+			Algo::Sort(movers, [this](const AActor* a1, const AActor* a2) {
+				return DistanceCompare(a1, a2);
+			});
+			//move towards nearest
+			FVector moveDir = movers[0]->GetActorLocation() - GetActorLocation();
+			SetActorLocation(GetActorLocation() + (moveDir * (moveSpeed * DeltaTime)));
+		}
+	}
+}
+
+void AMoverCube::MoveRandomOffset(float DeltaTime) {
+	SetActorLocation(GetActorLocation() + FVector(
+		FMath::RandRange(-randOffsetBounds.X, randOffsetBounds.X),
+		FMath::RandRange(-randOffsetBounds.Y, randOffsetBounds.Y),
+		FMath::RandRange(-randOffsetBounds.Z, randOffsetBounds.Z)
+		));
+}
+
+//============ sort helper func =============
+bool AMoverCube::DistanceCompare(const AActor* a1, const AActor* a2) {
+	return (GetActorLocation() - a1->GetActorLocation()).Length() < (GetActorLocation() - a2->GetActorLocation()).Length();
+}
--- UnrealCPP/Source/UnrealCPP/MoverCube.h
+++ UnrealCPP/Source/UnrealCPP/MoverCube.h
@@ -9,13 +9,22 @@ class UNREALCPP_API AMoverCube : public AActor
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Attributes)
+	float moveSpeed = 1.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Attributes)
+	FVector randOffsetBounds;
+
 public:	
 	AMoverCube();
+	virtual void Tick(float DeltaTime) override;
 
 protected:
 	virtual void BeginPlay() override;
 
-public:	
-	virtual void Tick(float DeltaTime) override;
+private:
+	void MoveToNearestNeighbour(float DeltaTime);
+	void MoveRandomOffset(float DeltaTime);
 
+	bool DistanceCompare(const AActor* a1, const AActor* a2);
 };
